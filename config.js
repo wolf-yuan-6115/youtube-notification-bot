5b30872c89d00dede832ce655a4b2f5689af5f4f
Wolf yuan <58245644+wolf-yuan-6115@users.noreply.github.com>
2021-06-24 12:44:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     token: "YOUR_DISCORD_BOT_TOKEN",
     channel: "XXXXXXXXXXXXXXXXXX",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel_id: ["YOUTUBE_CHANNEL_ID_1", "YOUTUBE_CHANNEL_ID_2"],
     watchInterval: 30000
 }
