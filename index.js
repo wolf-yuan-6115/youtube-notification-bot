17fd4e2657e30edb5550fd0f85525a96ad1cbcae
Wolf yuan <58245644+wolf-yuan-6115@users.noreply.github.com>
2021-06-24 12:47:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,25 +10,28 @@ client.on("ready", () => {
 });
 
 function handleUploads() {
+    client.config.channel.forEach(cID => {
     if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
-    setInterval(() => {
-        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
-        .then(data => {
-            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
-            else {
-                client.db.set(`videoData`, data.items[0]);
-                client.db.push("postedVideos", data.items[0].link);
-                let parsed = client.db.fetch(`videoData`);
-                let channel = client.channels.cache.get(client.config.channel);
-                if (!channel) return;
-                let message = client.config.messageTemplate
-                    .replace(/{author}/g, parsed.author)
-                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
-                    .replace(/{url}/g, parsed.link);
-                channel.send(message);
-            }
-        });
-    }, client.config.watchInterval);
+        setInterval(() => {
+            client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
+            .then(data => {
+                if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
+                else {
+                    client.db.set(`videoData`, data.items[0]);
+                    client.db.push("postedVideos", data.items[0].link);
+                    let parsed = client.db.fetch(`videoData`);
+                    let channel = client.channels.cache.get(cID);
+                    if (!channel) return;
+                    let message = client.config.messageTemplate
+                        .replace(/{author}/g, parsed.author)
+                        .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
+                        .replace(/{url}/g, parsed.link);
+                    channel.send(message);
+                }
+            });
+        }, client.config.watchInterval);
+    }
+    });
 }
 
 client.login(client.config.token);
